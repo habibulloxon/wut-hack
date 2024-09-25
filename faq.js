1cdc3a125e6dd9704f855cf51e9ea8398e036c8f
habibulloxon <hxayrulloxojayev@webster.edu>
2024-09-25 21:35:50 +0500

    [feat]: adaptive added

--- faq.js
+++ faq.js
@@ -1,6 +1,7 @@
 const plusBtns = Array.from(document.querySelectorAll(".question"));
 const answers = Array.from(document.querySelectorAll(".answer"));
 const imgPlus = Array.from(document.querySelectorAll(".img-plus"));
+const plusCircles = Array.from(document.querySelectorAll(".plus"))
 
 plusBtns.forEach( (item,ind) => {
   let toggle = false;
@@ -9,10 +10,12 @@ plusBtns.forEach( (item,ind) => {
     let numB = plusBtns[ind].children[0];
     if (toggle === false) {
       imgPlus[ind].style.rotate = "45deg";
+      plusCircles[ind].style.top = "20%"
       numB.style.top = '20%';
       toggle = true;
     } else {
       imgPlus[ind].style.rotate = "0deg";
+      plusCircles[ind].style.top = "50%"
       numB.style.top = '50%';
       toggle = false;
     }
