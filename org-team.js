ea78fdc6f51fdab57b702be6e8f7038b093514ed
habibulloxon <hxayrulloxojayev@webster.edu>
2024-10-08 12:37:19 -0700

    [feat]: images added

--- org-team.js
+++ org-team.js
@@ -1,145 +1,25 @@
-let groupbuttons = Array.from(document.querySelectorAll(".org-tablet"));
-let images = document.querySelector(".org-images");
-let arrowButton1 = document.querySelector(".arrow-1");
-let arrowButton2 = document.querySelector(".arrow-2");
-let mfImage = document.getElementById("mf-img").getBoundingClientRect().width;;
+const tablets = [...document.querySelectorAll(".org-tablet")];
+const names = document.querySelector(".org-images");
 
-window.addEventListener('resize', function(event){
-  mfImage = document.getElementById("mf-img").getBoundingClientRect().width;
-});
-groupbuttons.forEach( (item, ind) => {
-  item.addEventListener("click", () => {
-    images.innerHTML = photos[ind];
-    let prebut = groupbuttons.find( elem => elem.classList.contains("active_tablet"));
-    prebut.classList.remove("active_tablet");
+tablets.forEach((item, ind) => {
+  item.onclick = () => {
+    tablets.forEach((elem) => elem.classList.remove("active_tablet"));
     item.classList.add("active_tablet");
-  })
-})
-
-arrowButton1.addEventListener('click', () => {
-  images.scrollLeft -= mfImage + 12;
+    names.innerHTML = orglist[ind];
+  };
 });
 
+let group1 = `      <div>
+        <img id="mf-img" src="images/HamidKhusanov-photography--5.jpg">
+    </div>`;
+let group2 = `      <div>
+        <img id="mf-img" src="images/HamidKhusanov-photography--3.jpg">
+    </div>`;
+let group3 = `      <div>
+        <img id="mf-img" src="images/HamidKhusanov-photography--2.jpg">
+    </div>`;
+let group4 = `      <div>
+        <img id="mf-img" src="images/HamidKhusanov-photography-.jpg">
+    </div>`;
 
-arrowButton2.addEventListener('click', () => {
-  images.scrollLeft += mfImage + 12;
-});
-
-let group1 = `<div>
-        <img src="images/image.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-      </div>
-      </div>
-      <div>
-        <img src="images/image.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-      </div>
-      </div>
-      <div>
-        <img src="images/image.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-      </div>
-      </div>
-      <div>
-        <img src="images/image.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-      </div>
-      </div>`
-let group2 = `</div>
-      <div>
-        <img src="images/image 2.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      </div>
-      <div>
-        <img src="images/image 2.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      </div>
-      <div>
-        <img src="images/image 2.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      </div>
-      <div>
-        <img src="images/image 2.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>`;
-let group3 =`<div>
-        <img src="images/image 3.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 3.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 3.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 3.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>`;
-let group4 = `<div>
-        <img src="images/image 4.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 4.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 4.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>
-      <div>
-        <img src="images/image 4.png"> 
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-          <p>Supervision</p>
-        </div>
-      </div>`;
-      
-let photos = [group1, group2, group3, group4];
+let orglist = [group1, group2, group3, group4];
