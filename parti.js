53c101486a9714fdba0f638464b6268b5b5f1a4e
habibulloxon <hxayrulloxojayev@webster.edu>
2024-11-07 08:56:48 +0500

    [feat]: participants photos added

--- parti.js
+++ parti.js
@@ -1,69 +1,26 @@
 let tabs = Array.from(document.querySelectorAll(".part-tab"));
-let image = document.querySelector(".team-images");
+let imageContainer = document.querySelector(".team-images");
 
-tabs.forEach((item,ind) => {
-  item.addEventListener("click", () => {
-    let activeTab = tabs.find( elem => elem.classList.contains("active_tab"));
-    activeTab.classList.remove("active_tab");
-    item.classList.add("active_tab");
-    image.innerHTML = imgGrp[ind];
-  })
+let imgGrp = [
+  `<img src="images/1.jpg">`,
+  `<img src="images/2.jpg">`,
+  `<img src="images/3.jpg">`,
+  `<img src="images/4.jpg">`,
+  `<img src="images/5.jpg">`,
+  `<img src="images/6.jpg">`,
+  `<img src="images/7.jpg">`,
+];
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    let activeTab = document.querySelector(".active_tab");
+    if (activeTab) activeTab.classList.remove("active_tab");
+    tab.classList.add("active_tab");
+
+    imageContainer.style.opacity = 0;
+    setTimeout(() => {
+      imageContainer.innerHTML = imgGrp[index];
+      imageContainer.style.opacity = 1;
+    }, 300);
+  });
 });
-
-let imgGrp = [`<img src="images/Rectangle 49.png">
-      <div class="participants-names">
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-      </div>`, `<img src="images/Rectangle 42.png">
-      <div class="participants-names">
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-      </div>`, `<img src="images/Rectangle 49.png">
-      <div class="participants-names">
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-      </div>`, `<img src="images/Rectangle 42.png">
-      <div class="participants-names">
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-        <div>
-          <h4>Dr. Nitin Goje</h4>
-        </div>
-      </div>`]
